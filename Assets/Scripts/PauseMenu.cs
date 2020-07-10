eccc958067d5ea7a0c98930c7f6218aa1b2e4a1c
Vinicio <viniciobb@users.noreply.github.com>
2020-07-10 12:17:56 -0300

    icons

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    
+    // Update is called once per frame
+    
+
+    public void Resume()
+    {
+        Debug.Log("Resumed");
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Paused");
+        Time.timeScale = 0f; // freeses de game
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
